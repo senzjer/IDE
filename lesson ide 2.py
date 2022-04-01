d9e815bd82e9ce78178fd395462ac2fe24ba1d0c
senzjer <102347353+senzjer@users.noreply.github.com>
2022-04-01 08:24:41 +0300

    Update lesson ide 2.py

--- lesson ide 2.py
+++ lesson ide 2.py
@@ -1,3 +1,22 @@
+import random
+
+def try_next_move():
+    if random.random() < 0.25:
+        print("try to go right")
+
+    elif random.random() < 0.5:
+        print("try to go left")
+
+    elif random.random() < 0.75:
+        print("try to go bottom")
+
+    elif random.random() < 1:
+        print("try to go top")
+
+try_next_move()
+try_next_move()
+try_next_move()
+
 map1 =  [
     [12, 0, 1, 0],
     [1, 0, 1, 0],
@@ -11,10 +30,10 @@ map2 = [
     [0, 0, 0, 24]
 ]
 map3 = [
-    [12, 1, 0, 0],
-    [0, 0, 1, 0],
-    [1, 0, 1, 0],
-    [24, 0, 0, 0]
+    [12, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0],
+    [24, 0, 0, 0, 0, 0]
 ]
 map = map3
 
@@ -28,16 +47,15 @@ def move(step, current_position_x, current_position_y):
     print("current_position_x = ", current_position_x)
     print("current_position_y = ", current_position_y)
 
-    can_move_right = current_position_x <= 2 and map[current_position_y][current_position_x+1] == 0
-    can_move_bottom = current_position_y <= 2 and map[current_position_y+1][current_position_x] == 0
-    can_move_left = current_position_x <= 2 and map[current_position_y][current_position_x-1] == 0
-    can_move_up = current_position_y <= 2 and map[current_position_y-1][current_position_x] == 0
+    can_move_right = current_position_x <= 4 and map[current_position_y][current_position_x+1] == 0
+    can_move_bottom = current_position_y <= 3 and map[current_position_y+1][current_position_x] == 0
+    can_move_left = current_position_x >= 1 and map[current_position_y][current_position_x-1] == 0
+    
 
-    right_is_finish = current_position_x <= 2 and map[current_position_y][current_position_x+1] == 24
-    bottom_is_finish = current_position_y <= 2 and map[current_position_y+1][current_position_x] == 24
-    left_is_finish = current_position_x <= 2 and map[current_position_x][current_position_x-1] == 24
-    up_is_finish = current_position_y <= 2 and map[current_position_y-1][current_position_x] == 24
-
+    right_is_finish = current_position_x <= 4 and map[current_position_y][current_position_x+1] == 24
+    bottom_is_finish = current_position_y <= 3 and map[current_position_y+1][current_position_x] == 24
+    left_is_finish = current_position_x >= 1 and map[current_position_y][current_position_x-1] == 24
+   
     if right_is_finish:
         print("Found finish on right")
         return False
@@ -45,13 +63,9 @@ def move(step, current_position_x, current_position_y):
     if bottom_is_finish:
         print("Found finish on bottom")
         return False
-
-    if up_is_finish:
-        print("Found finish on up")
-        return False
     
     if left_is_finish:
-        print("Found left on right")
+        print("Found finish on left")
         return False
 
     if can_move_right:
@@ -64,11 +78,7 @@ def move(step, current_position_x, current_position_y):
     
     if can_move_left:
         print("Should move left")
-        return[current_position_y][current_position_x-1]
-
-    if can_move_up:
-        print("Should move up")
-        return[current_position_y-1][current_position_x]
+        return[current_position_y][current_position_x - 1]
 
 current_move_number = 1
 new_position = move(current_move_number, start_position_x, start_position_y)
